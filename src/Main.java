381fe650e8ac995e7293a608bce90021fdcf4879
Artur Ezhov <enproms@yandex.ru>
2023-04-09 17:11:54 +0500

    modern

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 13676;
-        int miles = price / 20;
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 13_676;
+        int miles = service.calculate(price);
         System.out.println("Бонус: " + miles);
     }
 }
